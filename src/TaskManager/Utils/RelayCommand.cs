1fd686851d943e42acb4d28dc892a34e12f889c8
TwiWeYo <portportpor@gmail.com>
2026-03-15 16:54:04 +0300

    feat: relay command

---
+++ src/TaskManager/Utils/RelayCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace TaskManager.Utils
+{
+    public class RelayCommand(Action<object?> execute, Func<object?, bool> canExecute) : ICommand
+    {
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? false;
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute?.Invoke(parameter);
+        }
+    }
+}
