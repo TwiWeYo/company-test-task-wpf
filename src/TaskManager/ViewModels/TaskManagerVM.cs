5462ab22e639447b761f39401c7a4cbb4b0f27be
TwiWeYo <portportpor@gmail.com>
2026-03-15 16:54:23 +0300

    feat: test implementation of viewmodel

---
+++ src/TaskManager/ViewModels/TaskManagerVM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using TaskManager.Model;
+using TaskManager.Utils;
+
+namespace TaskManager.ViewModels;
+public class TaskManagerVM
+{
+    public ObservableCollection<BusinessTask> Tasks { get; }
+    public readonly IEnumerable<string> Statuses = Enum.GetNames<Status>();
+
+    private RelayCommand _addCommand;
+    public RelayCommand AddCommand => _addCommand ??= new(q => MessageBox.Show(q.ToString()), q => true);
+    
+    private RelayCommand _removeCommand;
+    public RelayCommand RemoveCommand => _removeCommand ??= new(q => Tasks.Remove(q as BusinessTask), q => Tasks?.Any() == true);
+
+    private RelayCommand _updateCommand;
+    public RelayCommand UpdateCommand => _updateCommand ??= new(q => MessageBox.Show(q?.ToString()), q => Tasks.Any() == true);
+
+    public TaskManagerVM()
+    {
+        Tasks = new()
+        {
+            new BusinessTask { Title = "Тест", Description = "Посмотреть как оно будет выглядеть", DueDate = DateTime.Now, Priority = Priority.High, Status = Status.Active },
+            new BusinessTask { Title = "Тест", Description = "Посмотреть как оно будет выглядеть", DueDate = DateTime.Now, Priority = Priority.High, Status = Status.Active },
+            new BusinessTask { Title = "Тест", Description = "Посмотреть как оно будет выглядеть", DueDate = DateTime.Now, Priority = Priority.High, Status = Status.Active },
+            new BusinessTask { Title = "Тест", Description = "Посмотреть как оно будет выглядеть", DueDate = DateTime.Now, Priority = Priority.High, Status = Status.Active },
+            new BusinessTask { Title = "Тест", Description = "Посмотреть как оно будет выглядеть", DueDate = DateTime.Now, Priority = Priority.High, Status = Status.Active },
+            new BusinessTask { Title = "Тест", Description = "Посмотреть как оно будет выглядеть", DueDate = DateTime.Now, Priority = Priority.High, Status = Status.Active },
+            new BusinessTask { Title = "Тест", Description = "Посмотреть как оно будет выглядеть", DueDate = DateTime.Now, Priority = Priority.High, Status = Status.Active }
+        };
+    }
+
+}
